0a4c371b775615dbba57bfb2f004f976072e7b19
Chloe3c <kristianishenko21@gmail.com>
2024-11-14 23:42:14 +0200

    Добавьте файлы проекта.

---
+++ LABA4.1/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] array = { 2, 4, 6, 8, 10 };
+
+        if (IsArithmeticProgression(array))
+        {
+            Console.WriteLine("Образуют арифметическую прогрессию.");
+        }
+        else
+        {
+            Console.WriteLine("Не образуют арифметическую прогрессию.");
+        }
+    }
+
+    static bool IsArithmeticProgression(int[] array)
+    {
+        if (array.Length < 2) return false;
+
+        int difference = array[1] - array[0];
+
+        for (int i = 1; i < array.Length - 1; i++)
+        {
+            if (array[i + 1] - array[i] != difference)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
